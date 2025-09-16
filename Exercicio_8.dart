9a48596085acc447da1ff34cb4718b62b97954ca
Duane0307 <duduane02@hotmail.com>
2025-09-16 23:52:27 +0000

    feat: add projeto

---
+++ Exercicio_8.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  print('Digite o lado A:');
+  double a = double.parse(stdin.readLineSync()!);
+
+  print('Digite o lado B:');
+  double b = double.parse(stdin.readLineSync()!);
+
+  print('Digite o lado C:');
+  double c = double.parse(stdin.readLineSync()!);
+
+  // Verifica desigualdade triangular
+  if (a + b > c && a + c > b && b + c > a) {
+    if (a == b && b == c) {
+      print('Triângulo Equilátero');
+    } else if (a == b || a == c || b == c) {
+      print('Triângulo Isósceles');
+    } else {
+      print('Triângulo Escaleno');
+    }
+  } else {
+    print('Os valores não formam um triângulo');
+  }
+}
